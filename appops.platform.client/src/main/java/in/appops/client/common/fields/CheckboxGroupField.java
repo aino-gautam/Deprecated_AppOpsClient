f2824029e14a729c1af512509d917c28bc67a7d0
nairutee <nairutee@ensarm.com>
2013-03-11 16:42:09 +0530

    created new fields

---
+++ appops.platform.client/src/main/java/in/appops/client/common/fields/CheckboxGroupField.java
@@ -0,0 +1,65 @@
+package in.appops.client.common.fields;
+
+import in.appops.client.common.event.handlers.FieldEventHandler;
+import in.appops.platform.core.shared.Configuration;
+import in.appops.platform.core.util.AppOpsException;
+
+import com.google.gwt.event.shared.GwtEvent.Type;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.Composite;
+
+public class CheckboxGroupField extends Composite implements Field{
+
+	private Configuration configuration;
+	private String fieldValue;
+	
+	public CheckboxGroupField(){
+		
+	}
+	
+	@Override
+	public Configuration getConfiguration() {
+		return this.configuration;
+	}
+
+	@Override
+	public void setConfiguration(Configuration conf) {
+		this.configuration = conf;
+	}
+
+	@Override
+	public void createField() throws AppOpsException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clearField() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resetField() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getFieldValue() {
+		return this.fieldValue;
+	}
+
+	@Override
+	public void setFieldValue(String fieldValue) {
+		this.fieldValue = fieldValue;
+	}
+
+	@Override
+	public HandlerRegistration addFieldHandler(FieldEventHandler handler,
+			Type<FieldEventHandler> type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
