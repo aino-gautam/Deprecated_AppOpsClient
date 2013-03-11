f160f73f62c142c5571cc66ef87ae4aa6237bd53
nairutee <nairutee@ensarm.com>
2013-03-11 16:41:37 +0530

    added the fieldeventhandler

--- appops.platform.client/src/main/java/in/appops/client/common/fields/Field.java
+++ appops.platform.client/src/main/java/in/appops/client/common/fields/Field.java
@@ -1,5 +1,9 @@
 package in.appops.client.common.fields;
 
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.event.shared.GwtEvent.Type;
+
+import in.appops.client.common.event.handlers.FieldEventHandler;
 import in.appops.platform.core.shared.Configurable;
 import in.appops.platform.core.util.AppOpsException;
 
@@ -14,5 +18,7 @@ public interface Field extends Configurable {
 	public String getFieldValue();
 	
 	public void setFieldValue(String fieldValue);
+	
+	public HandlerRegistration addFieldHandler(FieldEventHandler handler, Type<FieldEventHandler> type);
 	
 }
