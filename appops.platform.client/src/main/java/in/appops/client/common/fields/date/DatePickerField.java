f3fa280fafe3793cd41c34a97da11ebfa35a81d9
nitish <nitish@ensarm.com>
2013-06-19 11:03:48 +0530

    Adding files for date picker field

--- appops.platform.client/src/main/java/in/appops/client/common/fields/date/DatePickerField.java
+++ appops.platform.client/src/main/java/in/appops/client/common/fields/date/DatePickerField.java
@@ -1,39 +1,53 @@
 package in.appops.client.common.fields.date;
 
-import in.appops.client.common.event.AppUtils;
 import in.appops.client.common.event.FieldEvent;
-import in.appops.client.common.fields.DateOnlyPicker;
 import in.appops.client.common.fields.Field;
 import in.appops.platform.core.shared.Configuration;
 import in.appops.platform.core.util.AppOpsException;
 
+import java.util.Date;
+
 import com.google.gwt.dom.client.Node;
 import com.google.gwt.dom.client.NodeList;
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.CloseEvent;
 import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.event.logical.shared.ShowRangeEvent;
+import com.google.gwt.event.logical.shared.ShowRangeHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.ToggleButton;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.datepicker.client.DatePicker;
+import com.google.gwt.user.datepicker.client.CalendarUtil;
 
 public class DatePickerField extends Composite implements Field, ClickHandler {
 	
+	public interface DatePickerConstant {
+
+	}
+	
 	private HorizontalPanel dtPickFieldBase;
+	private VerticalPanel vp;
 	private VerticalPanel dtPickPopupBase;
 	private PopupPanel dtPickPopup;
 	private TextBox dtPickBox;
 	private ToggleButton dtPickTrigger;
 	private AppopsDatePicker dtPicker;
-	
+	private Date start;
+	private Date end;
+	Label invalidFormat;
+	Date startDt = DateTimeFormat.getFormat("dd/MM/yyyy").parse("09/08/2013");
+	Date endDt = DateTimeFormat.getFormat("dd/MM/yyyy").parse("22/08/2013");
 	public DatePickerField(){
 		initialize();
 		
@@ -48,10 +62,12 @@ public class DatePickerField extends Composite implements Field, ClickHandler {
 	private void initialize() {
 		dtPickFieldBase = new HorizontalPanel();
 		dtPickBox=new TextBox();
+		dtPickBox.getElement().setAttribute("placeholder", "DD/MM/YYYY");
 		dtPickTrigger = new ToggleButton();
 		dtPickPopupBase = new VerticalPanel();
 		dtPickPopup = new PopupPanel(true);
 		dtPicker = new AppopsDatePicker();
+		vp = new VerticalPanel();
 	}
 	
 	public void configure() {
@@ -60,13 +76,22 @@ public class DatePickerField extends Composite implements Field, ClickHandler {
 	
 	@Override
 	public void create() throws AppOpsException {
+		Date dateString = DateTimeFormat.getFormat("dd/MM/yyyy").parse("12/08/2013");
+        dtPickBox.setText( DateTimeFormat.getFormat("dd/MM/yyyy").format(dateString));
+        vp.add(dtPickFieldBase);
 		dtPickFieldBase.setStylePrimaryName(getDatePickFieldPrimCss());
 		dtPickBox.setStylePrimaryName(getDatePickBoxPrimCss());
 		dtPickFieldBase.add(dtPickBox);
 		
 		dtPickTrigger.setStylePrimaryName("appops-dtPickTrigger");
 		dtPickFieldBase.add(dtPickTrigger);
-		
+		invalidFormat = new Label("Invalid format");
+		invalidFormat.setWidth("150px");
+		invalidFormat.setHeight("20px");
+		invalidFormat.setStylePrimaryName("appops-ErrorText");
+		vp.add(invalidFormat);
+		dtPickTrigger.setDown(false);
+		invalidFormat.setVisible(false);
 		NodeList<com.google.gwt.dom.client.Element> nodeList = dtPickFieldBase.getElement().getElementsByTagName("td");
 	    Node td1Node = nodeList.getItem(0);
 	    Element td1Element = (Element) Element.as(td1Node);
@@ -81,7 +106,7 @@ public class DatePickerField extends Composite implements Field, ClickHandler {
 	    dtPickPopup.setAnimationEnabled(true);
 	    dtPickPopup.setWidget(dtPicker);
 	    
-		initWidget(dtPickFieldBase);
+		initWidget(vp);
 		dtPickTrigger.addClickHandler(this);
 		dtPicker.addValueChangeHandler(new ValueChangeHandler(){
 			@Override
@@ -101,8 +126,49 @@ public class DatePickerField extends Composite implements Field, ClickHandler {
 				
 			}
 		});
+		
+		dtPicker.addShowRangeHandler(new ShowRangeHandler<Date>() {
+	         public void onShowRange(ShowRangeEvent<Date> dateShowRangeEvent) {
+	             setValidDates(dateShowRangeEvent);
+	         }
+	    });
+		dtPickBox.addBlurHandler(new BlurHandler() {
+			
+			@Override
+			public void onBlur(BlurEvent event) {
+				p();
+				
+			}
+		});
 	}
 
+	private void setValidDates(ShowRangeEvent<Date> dateShowRangeEvent) {
+        start = dateShowRangeEvent.getStart();
+        end = dateShowRangeEvent.getEnd();
+
+        Integer daysBetween = CalendarUtil.getDaysBetween(start, end);
+
+        for (int i = 0; i < daysBetween; i++) {
+            Date date = new Date(start.getTime());
+            CalendarUtil.addDaysToDate(date, i);
+            setDatePickable(date);
+        }
+    }
+    private void setDatePickable(Date date) {
+        Boolean enabled = true;
+        Date todaysDate = new Date();
+//        String dateString = DateTimeFormat.getFormat("dd/MM/yyyy").format(todaysDate);
+//        Date parsedDate = DateTimeFormat.getFormat("dd/MM/yyyy").parse(dateString);
+//        Date endDate = DateTimeFormat.getFormat("dd/MM/yyyy").parse("22/08/2013");
+        if (date.before(startDt)) {
+            enabled = false;
+        } else if (date.after(endDt)) {
+            enabled = false;
+        }
+        dtPicker.getCalendarView().setEnabledOnDate(enabled, date);
+    }
+	
+	
 	@Override
 	public Configuration getConfiguration() {
 		// TODO Auto-generated method stub
@@ -203,7 +269,11 @@ public class DatePickerField extends Composite implements Field, ClickHandler {
 	public void onClick(ClickEvent event) {
 		if(event.getSource().equals(dtPickTrigger)) {
 			if(dtPickTrigger.isDown()) {
-				dtPickPopup.showRelativeTo(dtPickBox);
+/*				final String[] formats = { "yyyy-MM-dd'T'HH:mm:ss'Z'", "yyyy-MM-dd'T'HH:mm:ssZ", "yyyy-MM-dd'T'HH:mm:ss",
+                    "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "yyyy-MM-dd'T'HH:mm:ss.SSSZ", "yyyy-MM-dd HH:mm:ss", "MM/dd/yyyy HH:mm:ss",
+                    "MM/dd/yyyy'T'HH:mm:ss.SSS'Z'", "MM/dd/yyyy'T'HH:mm:ss.SSSZ", "MM/dd/yyyy'T'HH:mm:ss.SSS", "MM/dd/yyyy'T'HH:mm:ssZ",
+                    "MM/dd/yyyy'T'HH:mm:ss", "yyyy:MM:dd HH:mm:ss", "yyyyMMdd"};
+*/				p();
 			} else if(!dtPickTrigger.isDown()) {
 
 			}
@@ -212,6 +282,55 @@ public class DatePickerField extends Composite implements Field, ClickHandler {
 		
 	}
 	
-	
+	public void p () {
+		final String[] formats = { "dd.MM.yyyy", "dd/MM/yyyy", "dd-MM-yyyy"};
+		String regex = "^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\\d\\d$";
+		Date endDate = null;
+		boolean parsed = false;
+		if(!dtPickBox.getText().equals("")) {
+			for(String parse : formats) {
+				try {
+					if(!parsed) { 
+						endDate = DateTimeFormat.getFormat(parse).parse(dtPickBox.getText());
+						if(endDate != null)
+							parsed = true;
+					}
+				} catch(IllegalArgumentException e) {
+					System.out.println("sdfsd");
+				}
+			}
+			if(parsed) {
+				if(dtPickBox.getText().matches(regex)) {
+					if(endDate.before(endDt) && endDate.after(startDt)) {
+						dtPicker.setCurrentMonth(endDate);
+						dtPicker.setValue(endDate);
+						dtPickPopup.showRelativeTo(dtPickBox);
+						
+						invalidFormat.setVisible(false);
+					} else {
+						invalidFormat.setText("Date should be between max and min");
+						invalidFormat.setVisible(true);
+						dtPickTrigger.setDown(false);
+					}
+				} else {
+					invalidFormat.setText("Invalid date");
+					invalidFormat.setVisible(true);
+					dtPickTrigger.setDown(false);
+				}
+			} else {
+				invalidFormat.setText("Invalid format");
+				invalidFormat.setVisible(true);
+				dtPickTrigger.setDown(false);
+			}
+		} else {
+			Date dateString = DateTimeFormat.getFormat("dd/MM/yyyy").parse("12/08/2013");
+			dtPicker.setCurrentMonth(endDate);
+
+			dtPicker.setValue(dateString);
+			dtPickPopup.showRelativeTo(dtPickBox);
+			
+			invalidFormat.setVisible(false);
+		}
+	}
 	
 }
