b6792505dd46c46ac3c77669620b0c29359e6ad2
test11 <test11@test11-desktop>
2013-07-24 16:34:26 +0530

    Added try catch and loggers in each method

--- appops.platform.client/src/main/java/in/appops/client/common/config/field/spinner/ListModel.java
+++ appops.platform.client/src/main/java/in/appops/client/common/config/field/spinner/ListModel.java
@@ -1,13 +1,15 @@
 package in.appops.client.common.config.field.spinner;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ListModel implements SpinnerModel {
 	private ArrayList valueList;
 	private Object value;
 	private int currentIndex;
 	private boolean circular;
-	
+	private Logger logger = Logger.getLogger(getClass().getName());
 	
 	@Override
 	public void setValue(Object value) {
@@ -22,26 +24,36 @@ public class ListModel implements SpinnerModel {
 
 	@Override
 	public void spinUp() {
-		if(getCurrentIndex() + 1 < getValueList().size()) {
-			setCurrentIndex(getCurrentIndex() + 1);
-		} else {
-			if(isCircular()) {
-				setCurrentIndex(0);
-			} 
+		try {
+			logger.log(Level.INFO, "[ListModel] ::In spinUp method ");
+			if(getCurrentIndex() + 1 < getValueList().size()) {
+				setCurrentIndex(getCurrentIndex() + 1);
+			} else {
+				if(isCircular()) {
+					setCurrentIndex(0);
+				} 
+			}
+			value = valueList.get(getCurrentIndex());
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "[ListModel] ::Exception in spinUp method :"+e);
 		}
-		value = valueList.get(getCurrentIndex());
 	}
 
 	@Override
 	public void spinDown() {
-		if(getCurrentIndex() - 1 >= 0) {
-			setCurrentIndex(getCurrentIndex() - 1);
-		} else {
-			if(isCircular()) {
-				setCurrentIndex(getValueList().size() - 1);
-			} 
+		try {
+			logger.log(Level.INFO, "[ListModel] ::In spinDown method ");
+			if(getCurrentIndex() - 1 >= 0) {
+				setCurrentIndex(getCurrentIndex() - 1);
+			} else {
+				if(isCircular()) {
+					setCurrentIndex(getValueList().size() - 1);
+				} 
+			}
+			value = valueList.get(getCurrentIndex());
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "[ListModel] ::Exception in spinDown method :"+e);
 		}
-		value = valueList.get(getCurrentIndex());
 	}
 
 	@Override
