20a7cd9f806058f378e3e5aeec8f3f1c131a6d02
test11 <test11@test11-desktop>
2013-07-24 16:33:06 +0530

    Added try catch and loggers in each method

--- appops.platform.client/src/main/java/in/appops/client/common/config/field/rangeslider/RangeSliderField.java
+++ appops.platform.client/src/main/java/in/appops/client/common/config/field/rangeslider/RangeSliderField.java
@@ -3,6 +3,8 @@ package in.appops.client.common.config.field.rangeslider;
 import in.appops.client.common.config.field.BaseField;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -36,7 +38,8 @@ public class RangeSliderField extends BaseField{
 	private VerticalPanel basePanel ;
 	private NumericRangeSlider numericRngeSlider;
 	private StringRangeSlider strRangeSlider;
-	
+	private Logger logger = Logger.getLogger(getClass().getName());
+
 	public RangeSliderField(){
 		
 	}
@@ -45,50 +48,65 @@ public class RangeSliderField extends BaseField{
 
 	@Override
 	public void create() {
-		getBasePanel().add(basePanel, DockPanel.CENTER);
+		try {
+			logger.log(Level.INFO, "[RangeSliderField] ::In create method ");
+			getBasePanel().add(basePanel, DockPanel.CENTER);
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "[RangeSliderField] ::Exception in create method :"+e);
+		}
 	}
 	
 	@Override
 	public void configure() {
 		
-		basePanel = new VerticalPanel();
-		
-		String mode = getRangeSliderMode();
-		
-		if (mode.equals(RangeSliderFieldConstant.NUM_SLIDER)) {
+		try {
+			logger.log(Level.INFO, "[RangeSliderField] ::In configure method ");
+			basePanel = new VerticalPanel();
 			
-			numericRngeSlider = new NumericRangeSlider(getMinValue(),getMaxValue());
-			numericRngeSlider.setStepSize(getStepValue());
-			numericRngeSlider.setCurrentValue(getMinValue());
-			numericRngeSlider.setSliderBarLineCss(getSliderLineCss());
-			numericRngeSlider.createUI();
+			String mode = getRangeSliderMode();
 			
-			basePanel.add(numericRngeSlider);
-		}else if(mode.equals(RangeSliderFieldConstant.STRING_SLIDER)) {
-			strRangeSlider = new StringRangeSlider(1,getRangeSliderItemList().size());
-			strRangeSlider.setListOfOption(getRangeSliderItemList());
-			strRangeSlider.setSliderBarLineCss(getSliderLineCss());
-			strRangeSlider.setStepSize(1);
-			strRangeSlider.setCurrentValue(1);
-			strRangeSlider.createUI();
-			basePanel.add(strRangeSlider);
+			if (mode.equals(RangeSliderFieldConstant.NUM_SLIDER)) {
+				
+				numericRngeSlider = new NumericRangeSlider(getMinValue(),getMaxValue());
+				numericRngeSlider.setStepSize(getStepValue());
+				numericRngeSlider.setCurrentValue(getMinValue());
+				numericRngeSlider.setSliderBarLineCss(getSliderLineCss());
+				numericRngeSlider.createUI();
+				
+				basePanel.add(numericRngeSlider);
+			}else if(mode.equals(RangeSliderFieldConstant.STRING_SLIDER)) {
+				strRangeSlider = new StringRangeSlider(1,getRangeSliderItemList().size());
+				strRangeSlider.setListOfOption(getRangeSliderItemList());
+				strRangeSlider.setSliderBarLineCss(getSliderLineCss());
+				strRangeSlider.setStepSize(1);
+				strRangeSlider.setCurrentValue(1);
+				strRangeSlider.createUI();
+				basePanel.add(strRangeSlider);
+			}
+			
+			basePanel.setSpacing(10);
+			
+			if(getBaseFieldPrimCss() != null)
+				basePanel.setStylePrimaryName(getBaseFieldPrimCss());
+			if(getBaseFieldCss() != null)
+				basePanel.addStyleName(getBaseFieldCss());
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "[RangeSliderField] ::Exception in configure method :"+e);
 		}
-		
-		basePanel.setSpacing(10);
-		
-		if(getBaseFieldPrimCss() != null)
-			basePanel.setStylePrimaryName(getBaseFieldPrimCss());
-		if(getBaseFieldCss() != null)
-			basePanel.addStyleName(getBaseFieldCss());
 	}
 	
 	@Override
 	public Object getValue() {
 		
-		if(getRangeSliderMode().equals(RangeSliderFieldConstant.NUM_SLIDER)){
-			return numericRngeSlider.getCurrentValue();
-		}else if(getRangeSliderMode().equals(RangeSliderFieldConstant.STRING_SLIDER)){
-			return getRangeSliderItemList().get(((int)strRangeSlider.getCurrentValue())-1);
+		try {
+			logger.log(Level.INFO, "[RangeSliderField] ::In getValue method ");
+			if(getRangeSliderMode().equals(RangeSliderFieldConstant.NUM_SLIDER)){
+				return numericRngeSlider.getCurrentValue();
+			}else if(getRangeSliderMode().equals(RangeSliderFieldConstant.STRING_SLIDER)){
+				return getRangeSliderItemList().get(((int)strRangeSlider.getCurrentValue())-1);
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "[RangeSliderField] ::Exception in getValue method :"+e);
 		}
 		return null;
 	}
@@ -103,9 +121,14 @@ public class RangeSliderField extends BaseField{
 		
 		String mode = RangeSliderFieldConstant.NUM_SLIDER;
 		
-		if(getConfigurationValue(RangeSliderFieldConstant.SLIDER_MODE) != null) {
-			
-			mode = getConfigurationValue(RangeSliderFieldConstant.SLIDER_MODE).toString();
+		try {
+			logger.log(Level.INFO, "[RangeSliderField] ::In getRangeSliderMode method ");
+			if(getConfigurationValue(RangeSliderFieldConstant.SLIDER_MODE) != null) {
+				
+				mode = getConfigurationValue(RangeSliderFieldConstant.SLIDER_MODE).toString();
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "[RangeSliderField] ::Exception in getRangeSliderMode method :"+e);
 		}
 		return mode;
 	}
@@ -118,9 +141,14 @@ public class RangeSliderField extends BaseField{
 		
 		String css = "gwt-SliderBar-line";
 		
-		if(getConfigurationValue(RangeSliderFieldConstant.SLIDER_LINE_CSS) != null) {
-			
-			css = getConfigurationValue(RangeSliderFieldConstant.SLIDER_LINE_CSS).toString();
+		try {
+			logger.log(Level.INFO, "[RangeSliderField] ::In getSliderLineCss method ");
+			if(getConfigurationValue(RangeSliderFieldConstant.SLIDER_LINE_CSS) != null) {
+				
+				css = getConfigurationValue(RangeSliderFieldConstant.SLIDER_LINE_CSS).toString();
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "[RangeSliderField] ::Exception in getSliderLineCss method :"+e);
 		}
 		return css;
 	}
@@ -133,9 +161,14 @@ public class RangeSliderField extends BaseField{
 		
 		Double minVal = 0d;
 		
-		if(getConfigurationValue(RangeSliderFieldConstant.MINVAL) != null) {
-			
-			minVal = (Double)getConfigurationValue(RangeSliderFieldConstant.MINVAL);
+		try {
+			logger.log(Level.INFO, "[RangeSliderField] ::In getMinValue method ");
+			if(getConfigurationValue(RangeSliderFieldConstant.MINVAL) != null) {
+				
+				minVal = (Double)getConfigurationValue(RangeSliderFieldConstant.MINVAL);
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "[RangeSliderField] ::Exception in getMinValue method :"+e);
 		}
 		return minVal;
 	}
@@ -148,9 +181,14 @@ public class RangeSliderField extends BaseField{
 		
 		Double maxVal = 100d;
 		
-		if(getConfigurationValue(RangeSliderFieldConstant.MAXVAL) != null) {
-			
-			maxVal = (Double)getConfigurationValue(RangeSliderFieldConstant.MAXVAL);
+		try {
+			logger.log(Level.INFO, "[RangeSliderField] ::In getMaxValue method ");
+			if(getConfigurationValue(RangeSliderFieldConstant.MAXVAL) != null) {
+				
+				maxVal = (Double)getConfigurationValue(RangeSliderFieldConstant.MAXVAL);
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "[RangeSliderField] ::Exception in getMaxValue method :"+e);
 		}
 		return maxVal;
 	}
@@ -163,9 +201,14 @@ public class RangeSliderField extends BaseField{
 		
 		Double stepVal = 20d;
 		
-		if(getConfigurationValue(RangeSliderFieldConstant.STEPVAL) != null) {
-			
-			stepVal = (Double)getConfigurationValue(RangeSliderFieldConstant.STEPVAL);
+		try {
+			logger.log(Level.INFO, "[RangeSliderField] ::In getStepValue method ");
+			if(getConfigurationValue(RangeSliderFieldConstant.STEPVAL) != null) {
+				
+				stepVal = (Double)getConfigurationValue(RangeSliderFieldConstant.STEPVAL);
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "[RangeSliderField] ::Exception in getStepValue method :"+e);
 		}
 		return stepVal;
 	}
@@ -176,8 +219,13 @@ public class RangeSliderField extends BaseField{
 	 */
 	private ArrayList<String> getRangeSliderItemList() {
 		ArrayList<String> listOfItems = null;
-		if(getConfigurationValue(RangeSliderFieldConstant.ITEMS_LIST) != null) {
-			listOfItems = (ArrayList<String>) getConfigurationValue(RangeSliderFieldConstant.ITEMS_LIST);
+		try {
+			logger.log(Level.INFO, "[RangeSliderField] ::In getRangeSliderItemList method ");
+			if(getConfigurationValue(RangeSliderFieldConstant.ITEMS_LIST) != null) {
+				listOfItems = (ArrayList<String>) getConfigurationValue(RangeSliderFieldConstant.ITEMS_LIST);
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "[RangeSliderField] ::Exception in getRangeSliderItemList method :"+e);
 		}
 		return listOfItems;
 	}
