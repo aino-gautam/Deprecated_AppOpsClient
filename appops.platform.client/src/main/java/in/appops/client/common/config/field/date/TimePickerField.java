fa526ca4ee929a46d811048387903b6b1bc5213b
test11 <test11@test11-desktop>
2013-07-24 16:29:41 +0530

    Added try catch and loggers in each method

--- appops.platform.client/src/main/java/in/appops/client/common/config/field/date/TimePickerField.java
+++ appops.platform.client/src/main/java/in/appops/client/common/config/field/date/TimePickerField.java
@@ -9,6 +9,8 @@ import in.appops.platform.core.shared.Configuration;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.user.client.ui.DockPanel;
@@ -21,83 +23,109 @@ public class TimePickerField extends BaseField{
 	private SpinnerField minuteSpinner;
 	private SpinnerField secondSpinner;
 	private SpinnerField formatSpinner;
-	
+	private Logger logger = Logger.getLogger(getClass().getName());
 	public TimePickerField() {
 	}
 	
 	@Override
 	public void create() {
-		getBasePanel().add(timePanel,DockPanel.CENTER);
+		
+		try {
+			logger.log(Level.INFO,"[TimePickerField]:: In create  method ");
+			getBasePanel().add(timePanel,DockPanel.CENTER);
+		} catch (Exception e) {
+			logger.log(Level.SEVERE,"[TimePickerField]::Exception In create  method :"+e);
+		}
 	}
 	
 	@Override
 	public void configure() {
 				
-		timePanel =new HorizontalPanel();
-		Date date = new Date();
-		
-		String timeFormat = getTimeFormat();
-		int hour = date.getHours();
-		
-		if(timeFormat.equalsIgnoreCase(TimePickerFieldConstant.FORMAT24HOUR_WITH_SECONDS) ||timeFormat.equalsIgnoreCase(TimePickerFieldConstant.FORMAT24HOUR_WITHOUT_SECONDS)){
-			hourSpinner = getSpinnerField(getSpinnerConfiguration(0F, 23F,hour));
-		}else if(timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITHOUT_SECONDS) ||timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITH_SECONDS)){
-			hourSpinner = getSpinnerField(getSpinnerConfiguration(1F, 12F,hour-12));
-		}
-		
-		minuteSpinner = getSpinnerField(getSpinnerConfiguration(0F, 59F,date.getMinutes()));
-		
-		timePanel.add(hourSpinner);
-		timePanel.add(getSeperator());
-		timePanel.add(minuteSpinner);
-		
-		if(timeFormat.equalsIgnoreCase(TimePickerFieldConstant.FORMAT24HOUR_WITH_SECONDS) ||timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITH_SECONDS)){
-			secondSpinner = getSpinnerField(getSpinnerConfiguration(0F, 59F,date.getSeconds()));
-			timePanel.add(getSeperator());
-			timePanel.add(secondSpinner);
-		}
-		
-		if(timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITHOUT_SECONDS) ||timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITH_SECONDS)){
+		try {
+			logger.log(Level.INFO,"[TimePickerField]:: In configure  method ");
+			timePanel =new HorizontalPanel();
+			Date date = new Date();
 			
-			formatSpinner = getSpinnerField(getAMPMSpinnerConfiguration(hour));
-			timePanel.add(formatSpinner);
+			String timeFormat = getTimeFormat();
+			int hour = date.getHours();
+			
+			if(timeFormat.equalsIgnoreCase(TimePickerFieldConstant.FORMAT24HOUR_WITH_SECONDS) ||timeFormat.equalsIgnoreCase(TimePickerFieldConstant.FORMAT24HOUR_WITHOUT_SECONDS)){
+				hourSpinner = getSpinnerField(getSpinnerConfiguration(0F, 23F,hour));
+			}else if(timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITHOUT_SECONDS) ||timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITH_SECONDS)){
+				hourSpinner = getSpinnerField(getSpinnerConfiguration(1F, 12F,hour-12));
+			}
+			
+			minuteSpinner = getSpinnerField(getSpinnerConfiguration(0F, 59F,date.getMinutes()));
+			
+			timePanel.add(hourSpinner);
+			timePanel.add(getSeperator());
+			timePanel.add(minuteSpinner);
+			
+			if(timeFormat.equalsIgnoreCase(TimePickerFieldConstant.FORMAT24HOUR_WITH_SECONDS) ||timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITH_SECONDS)){
+				secondSpinner = getSpinnerField(getSpinnerConfiguration(0F, 59F,date.getSeconds()));
+				timePanel.add(getSeperator());
+				timePanel.add(secondSpinner);
+			}
+			
+			if(timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITHOUT_SECONDS) ||timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITH_SECONDS)){
+				
+				formatSpinner = getSpinnerField(getAMPMSpinnerConfiguration(hour));
+				timePanel.add(formatSpinner);
+			}
+			
+			if(getBaseFieldPrimCss()!=null)
+				getBasePanel().setStylePrimaryName(getBaseFieldPrimCss());
+			
+			if(getBaseFieldCss()!=null)
+				getBasePanel().setStyleName(getBaseFieldCss());
+		} catch (Exception e) {
+			logger.log(Level.SEVERE,"[TimePickerField]::Exception In configure  method :"+e);
 		}
-		
-		if(getBaseFieldPrimCss()!=null)
-			getBasePanel().setStylePrimaryName(getBaseFieldPrimCss());
-		
-		if(getBaseFieldCss()!=null)
-			getBasePanel().setStyleName(getBaseFieldCss());
 		
 	}
 	
 	private LabelField getSeperator(){
 		LabelField seperator =new LabelField();
-		seperator.setConfiguration(getLabelConfiguration());
-		seperator.configure();
-		seperator.create();
+		try {
+			logger.log(Level.INFO,"[TimePickerField]:: In getSeperator  method ");
+			seperator.setConfiguration(getLabelConfiguration());
+			seperator.configure();
+			seperator.create();
+		} catch (Exception e) {
+			logger.log(Level.SEVERE,"[TimePickerField]::Exception In getSeperator  method :"+e);
+		}
 		return seperator;
 	}
 	
 	private SpinnerField getSpinnerField(Configuration configuration){
 		SpinnerField spinner = new SpinnerField();
-		spinner.setConfiguration(configuration);
-		spinner.configure();
-		spinner.create();
+		try {
+			logger.log(Level.INFO,"[TimePickerField]:: In getSpinnerField  method ");
+			spinner.setConfiguration(configuration);
+			spinner.configure();
+			spinner.create();
+		} catch (Exception e) {
+			logger.log(Level.SEVERE,"[TimePickerField]::Exception In getSpinnerField  method :"+e);
+		}
 		return spinner;
 	}
 	
 	@Override
 	public Object getValue(){
 		Date date = null;
-		String time = hourSpinner.getValue().toString()+":"+minuteSpinner.getValue().toString() + ":" +secondSpinner.getValue().toString();
-		
-		String timeFormat = getTimeFormat();
-		
-		if(timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITHOUT_SECONDS) ||timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITH_SECONDS)){
-			date = DateTimeFormat.getFormat(getTimeFormat()).parse(time+" "+ formatSpinner.getValue().toString());
-		}else{
-			date = DateTimeFormat.getFormat(getTimeFormat()).parse(time);
+		try {
+			logger.log(Level.INFO,"[TimePickerField]:: In getValue  method ");
+			String time = hourSpinner.getValue().toString()+":"+minuteSpinner.getValue().toString() + ":" +secondSpinner.getValue().toString();
+			
+			String timeFormat = getTimeFormat();
+			
+			if(timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITHOUT_SECONDS) ||timeFormat.equalsIgnoreCase(TimePickerFieldConstant.AMPMFORMAT_WITH_SECONDS)){
+				date = DateTimeFormat.getFormat(getTimeFormat()).parse(time+" "+ formatSpinner.getValue().toString());
+			}else{
+				date = DateTimeFormat.getFormat(getTimeFormat()).parse(time);
+			}
+		} catch (IllegalArgumentException e) {
+			logger.log(Level.SEVERE,"[TimePickerField]::Exception In getValue  method :"+e);
 		}
 		return date;
 	}
@@ -105,6 +133,7 @@ public class TimePickerField extends BaseField{
 	
 	@Override
 	public String getFieldValue() {
+		logger.log(Level.INFO,"[TimePickerField]:: In getFieldValue  method ");
 		String time = hourSpinner.getValue().toString()+":"+minuteSpinner.getValue().toString() + ":" +secondSpinner.getValue().toString();
 		
 		String timeFormat =  getTimeFormat();
@@ -120,17 +149,27 @@ public class TimePickerField extends BaseField{
 	
 	public String getTimeFormat() {
 		String format = TimePickerFieldConstant.AMPMFORMAT_WITH_SECONDS;
-		if(getConfigurationValue(TimePickerFieldConstant.TIME_FORMAT) != null) {
-			format = getConfigurationValue(TimePickerFieldConstant.TIME_FORMAT).toString();
+		try {
+			logger.log(Level.INFO,"[TimePickerField]:: In getTimeFormat  method ");
+			if(getConfigurationValue(TimePickerFieldConstant.TIME_FORMAT) != null) {
+				format = getConfigurationValue(TimePickerFieldConstant.TIME_FORMAT).toString();
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE,"[TimePickerField]::Exception In getTimeFormat  method :"+e);
 		}
 		return format;
 	}
 	
 	public String getSpinnerPrimaryCss(){
 		String primCss = "timeSpinner";
-		if(getConfigurationValue(TimePickerFieldConstant.TIMESPINNER_PCLS) != null) {
-			
-			primCss = getConfigurationValue(TimePickerFieldConstant.TIMESPINNER_PCLS).toString();
+		try {
+			logger.log(Level.INFO,"[TimePickerField]:: In getSpinnerPrimaryCss  method ");
+			if(getConfigurationValue(TimePickerFieldConstant.TIMESPINNER_PCLS) != null) {
+				
+				primCss = getConfigurationValue(TimePickerFieldConstant.TIMESPINNER_PCLS).toString();
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE,"[TimePickerField]::Exception In getSpinnerPrimaryCss  method :"+e);
 		}
 		return primCss;
 		
@@ -138,9 +177,14 @@ public class TimePickerField extends BaseField{
 	
 	public String getSpinnerDependentCss(){
 		String dependentCss = null;
-		if(getConfigurationValue(TimePickerFieldConstant.TIMESPINNER_DCLS) != null) {
-			
-			dependentCss = getConfigurationValue(TimePickerFieldConstant.TIMESPINNER_DCLS).toString();
+		try {
+			logger.log(Level.INFO,"[TimePickerField]:: In getSpinnerDependentCss  method ");
+			if(getConfigurationValue(TimePickerFieldConstant.TIMESPINNER_DCLS) != null) {
+				
+				dependentCss = getConfigurationValue(TimePickerFieldConstant.TIMESPINNER_DCLS).toString();
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE,"[TimePickerField]::Exception In getSpinnerDependentCss  method :"+e);
 		}
 		return dependentCss;
 		
@@ -148,14 +192,19 @@ public class TimePickerField extends BaseField{
 	
 	private Configuration getSpinnerConfiguration(Float minValue , Float maxvalue ,int defaultVal){
 		Configuration configuration = new Configuration();
-		configuration.setPropertyByName(SpinnerFieldConstant.SP_MAXVAL, maxvalue);
-		configuration.setPropertyByName(SpinnerFieldConstant.SP_MINVAL, minValue);
-		configuration.setPropertyByName(SpinnerFieldConstant.SP_CIRCULAR, true);
-		configuration.setPropertyByName(SpinnerFieldConstant.BF_PCLS, getSpinnerPrimaryCss());
-		configuration.setPropertyByName(SpinnerFieldConstant.BF_DCLS, getSpinnerDependentCss());
-		configuration.setPropertyByName(SpinnerFieldConstant.BF_DEFVAL, Float.parseFloat(String.valueOf(defaultVal)));
-		configuration.setPropertyByName(SpinnerFieldConstant.SP_TYPE, SpinnerFieldConstant.SP_TYPENUMERIC);
-		configuration.setPropertyByName(SpinnerFieldConstant.BF_ERRPOS, SpinnerFieldConstant.BF_BOTTOM);
+		try {
+			logger.log(Level.INFO,"[TimePickerField]:: In getSpinnerConfiguration  method ");
+			configuration.setPropertyByName(SpinnerFieldConstant.SP_MAXVAL, maxvalue);
+			configuration.setPropertyByName(SpinnerFieldConstant.SP_MINVAL, minValue);
+			configuration.setPropertyByName(SpinnerFieldConstant.SP_CIRCULAR, true);
+			configuration.setPropertyByName(SpinnerFieldConstant.BF_PCLS, getSpinnerPrimaryCss());
+			configuration.setPropertyByName(SpinnerFieldConstant.BF_DCLS, getSpinnerDependentCss());
+			configuration.setPropertyByName(SpinnerFieldConstant.BF_DEFVAL, Float.parseFloat(String.valueOf(defaultVal)));
+			configuration.setPropertyByName(SpinnerFieldConstant.SP_TYPE, SpinnerFieldConstant.SP_TYPENUMERIC);
+			configuration.setPropertyByName(SpinnerFieldConstant.BF_ERRPOS, SpinnerFieldConstant.BF_BOTTOM);
+		} catch (NumberFormatException e) {
+			logger.log(Level.SEVERE,"[TimePickerField]::Exception In getSpinnerConfiguration  method :"+e);
+		}
 		
 		return configuration;
 	}
@@ -163,28 +212,38 @@ public class TimePickerField extends BaseField{
 	private Configuration getLabelConfiguration(){
 		
 		Configuration conf = new Configuration();
-		conf.setPropertyByName(LabelFieldConstant.LBLFD_DISPLAYTXT, ":");
-		conf.setPropertyByName(LabelFieldConstant.BF_PCLS, "timeSeperator");
+		try {
+			logger.log(Level.INFO,"[TimePickerField]:: In getLabelConfiguration  method ");
+			conf.setPropertyByName(LabelFieldConstant.LBLFD_DISPLAYTXT, ":");
+			conf.setPropertyByName(LabelFieldConstant.BF_PCLS, "timeSeperator");
+		} catch (Exception e) {
+			logger.log(Level.SEVERE,"[TimePickerField]::Exception In getLabelConfiguration  method :"+e);
+		}
 		return conf;
 	}
 	
 	private Configuration getAMPMSpinnerConfiguration( int hours){
-		ArrayList<String> format = new ArrayList<String>();
-		format.add("AM");
-		format.add("PM");
 		Configuration configuration = new Configuration();
-		configuration.setPropertyByName(SpinnerFieldConstant.SP_TYPE, SpinnerFieldConstant.SP_TYPELIST);
-		configuration.setPropertyByName(SpinnerFieldConstant.BF_PCLS, getSpinnerPrimaryCss());
-		configuration.setPropertyByName(SpinnerFieldConstant.BF_DCLS, getSpinnerDependentCss());
-		if(hours >= 12){
-			configuration.setPropertyByName(SpinnerFieldConstant.BF_DEFVAL,"PM");
-		}else{
-			configuration.setPropertyByName(SpinnerFieldConstant.BF_DEFVAL,"AM");
+		try {
+			logger.log(Level.INFO,"[TimePickerField]:: In getAMPMSpinnerConfiguration  method ");
+			ArrayList<String> format = new ArrayList<String>();
+			format.add("AM");
+			format.add("PM");
+			configuration.setPropertyByName(SpinnerFieldConstant.SP_TYPE, SpinnerFieldConstant.SP_TYPELIST);
+			configuration.setPropertyByName(SpinnerFieldConstant.BF_PCLS, getSpinnerPrimaryCss());
+			configuration.setPropertyByName(SpinnerFieldConstant.BF_DCLS, getSpinnerDependentCss());
+			if(hours >= 12){
+				configuration.setPropertyByName(SpinnerFieldConstant.BF_DEFVAL,"PM");
+			}else{
+				configuration.setPropertyByName(SpinnerFieldConstant.BF_DEFVAL,"AM");
+			}
+			
+			configuration.setPropertyByName(SpinnerFieldConstant.SP_VALUELIST, format);
+			configuration.setPropertyByName(SpinnerFieldConstant.SP_VALUEIDX, 0);
+			configuration.setPropertyByName(SpinnerFieldConstant.SP_CIRCULAR, true);
+		} catch (Exception e) {
+			logger.log(Level.SEVERE,"[TimePickerField]::Exception In getAMPMSpinnerConfiguration  method :"+e);
 		}
-		
-		configuration.setPropertyByName(SpinnerFieldConstant.SP_VALUELIST, format);
-		configuration.setPropertyByName(SpinnerFieldConstant.SP_VALUEIDX, 0);
-		configuration.setPropertyByName(SpinnerFieldConstant.SP_CIRCULAR, true);
 		return configuration;
 	}
 	
