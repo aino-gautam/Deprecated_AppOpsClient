3147729f9501837146ab893e3759d0334ead1ed5
MaheshMore <mahesh@ensarm.com>
2013-08-30 19:34:44 +0530

    register widget new added

--- appops.platform.client/src/main/java/in/appops/showcase/web/gwt/registercomponent/client/RegisterComponent.java
+++ appops.platform.client/src/main/java/in/appops/showcase/web/gwt/registercomponent/client/RegisterComponent.java
@@ -1,12 +1,19 @@
 package in.appops.showcase.web.gwt.registercomponent.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class RegisterComponent implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		
-		
+		try{
+			RegisterComponentManager compManager = new RegisterComponentManager();
+			compManager.createUi();
+			RootPanel.get().add(compManager);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
