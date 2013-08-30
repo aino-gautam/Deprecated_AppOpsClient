b72f8a8519a15436c270f2285e394c03dfa45b51
pallavi-ensarm <pallavi@ensarm.com>
2013-08-30 14:39:04 +0530

    new registercomponent module added in showcase

---
+++ appops.platform.client/src/main/java/in/appops/showcase/web/gwt/registercomponent/client/RegisterComponent.java
@@ -0,0 +1,12 @@
+package in.appops.showcase.web.gwt.registercomponent.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class RegisterComponent implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		
+		
+	}
+}
