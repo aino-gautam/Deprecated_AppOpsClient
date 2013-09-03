a2b49626f61ec9162bd8f31533cfc3f544c517e3
pallavi-ensarm <pallavi@ensarm.com>
2013-09-03 12:16:57 +0530

    module renamed to componentconfiguration also related classes renamed

---
+++ appops.platform.client/src/main/java/in/appops/showcase/web/gwt/componentconfiguration/client/ComponentConfigurationManger.java
@@ -0,0 +1,19 @@
+package in.appops.showcase.web.gwt.componentconfiguration.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class ComponentConfigurationManger implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		try{
+			ConfigurationManagerHome confManagerHome = new ConfigurationManagerHome();
+			confManagerHome.createUi();
+			RootPanel.get("componentconfiguration").add(confManagerHome);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
