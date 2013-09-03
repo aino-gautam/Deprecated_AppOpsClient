58ded83c17bb196e6930192092cfb5e9956f2efa
pallavi-ensarm <pallavi@ensarm.com>
2013-09-03 12:20:24 +0530

    pagemanager added for managing app pages

---
+++ appops.platform.client/src/main/java/in/appops/showcase/web/gwt/componentconfiguration/client/page/PageManager.java
@@ -0,0 +1,18 @@
+package in.appops.showcase.web.gwt.componentconfiguration.client.page;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class PageManager extends Composite {
+	
+	private VerticalPanel basePanel;
+
+	public PageManager() {
+		initialize();
+	}
+
+	private void initialize() {
+		basePanel = new VerticalPanel();
+		initWidget(basePanel);
+	}
+}
