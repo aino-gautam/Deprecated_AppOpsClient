7431243dec1e6a75cf5e2602c3d557861d73494c
MaheshMore <mahesh@ensarm.com>
2013-10-03 19:50:34 +0530

    unwanted class removed

--- appops.platform.client/src/main/java/in/appops/client/common/config/component/breadcrumb/BreadCrumbPresenter.java
+++
@@ -1,7 +0,0 @@
-package in.appops.client.common.config.component.breadcrumb;
-
-import in.appops.client.common.config.dsnip.HTMLSnippetPresenter;
-
-public class BreadCrumbPresenter extends HTMLSnippetPresenter{
-
-}
