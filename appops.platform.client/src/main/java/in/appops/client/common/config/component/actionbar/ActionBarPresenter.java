17eb5985231f8094d5e0f8d18fd050f66f48a562
nitish <nitish@ensarm.com>
2013-10-08 19:09:52 +0530

    Removing unwanted empty classes

--- appops.platform.client/src/main/java/in/appops/client/common/config/component/actionbar/ActionBarPresenter.java
+++
@@ -1,7 +0,0 @@
-package in.appops.client.common.config.component.actionbar;
-
-import in.appops.client.common.config.dsnip.HTMLSnippetPresenter;
-
-public class ActionBarPresenter extends HTMLSnippetPresenter{
-
-}
--- appops.platform.client/src/main/java/in/appops/client/common/config/component/user/UserSnippet.java
+++
@@ -1,7 +0,0 @@
-package in.appops.client.common.config.component.user;
-
-import in.appops.client.common.config.dsnip.HTMLSnippetPresenter;
-
-public class UserSnippet extends HTMLSnippetPresenter{
-
-}
