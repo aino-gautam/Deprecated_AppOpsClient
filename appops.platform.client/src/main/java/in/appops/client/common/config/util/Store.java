ebdf63dec93abcda17ae2ff763eb7830de0f527c
nitish <nitish@ensarm.com>
2013-10-08 19:13:35 +0530

    Committing changes to store, apputil and dynamicsnippet entry point

--- appops.platform.client/src/main/java/in/appops/client/common/config/util/Store.java
+++ appops.platform.client/src/main/java/in/appops/client/common/config/util/Store.java
@@ -5,19 +5,60 @@ import in.appops.platform.core.shared.Configuration;
 
 import com.google.gwt.i18n.client.Dictionary;
 
+/**
+ * This class stores all the configuration for a page, description of snippets used in a page() and
+ * data that is pre-populated for a page.
+ * @author nitish@ensarm.com
+ */
 public class Store {
 	
+	/** To store configuration	**/
     private static Dictionary configStore;
+    
+    /** To store pre-populated data	**/
     private static Dictionary cacheStore; 
     
+    /** To store snippets used in a page **/
+    private static Dictionary snippetStore;
+    
+    /** Constants **/
+    private final static String PAGECONFIG = "pageconfig";
+    private final static String PAGECACHE = "pagecache";
+    private final static String PAGESNIPPET = "snippetStore";
+
+    /**
+     * This methods populates the respective store
+     */
     public static void loadStore() {
-    	configStore = Dictionary.getDictionary("pageconfig");
-    	cacheStore = Dictionary.getDictionary("pagecache");
+    	configStore = Dictionary.getDictionary(PAGECONFIG);
+    	cacheStore = Dictionary.getDictionary(PAGECACHE);
+    	snippetStore = Dictionary.getDictionary(PAGESNIPPET);
     }
  	
-	public static Configuration getConfiguration(String configId) {
+	public static Configuration getFromConfigurationStore(String configId) {
 		try {
 			String jsonConfig = configStore.get(configId);
+			JsonToEntityConverter convertor = new JsonToEntityConverter();
+			Configuration config = (Configuration) convertor.convertjsonStringToEntity(jsonConfig);
+			return config;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static Configuration getChildConfiguration(String instance, String dataConfig) {
+		Configuration parentConfig = getFromConfigurationStore(instance);
+		return (Configuration) parentConfig.getProperty(dataConfig);
+	}
+	
+	public static Configuration getChildConfiguration(Configuration conf, String dataConfig) {
+		return (Configuration) conf.getProperty(dataConfig);
+	}
+	
+	public static Configuration getFromCacheStore(String key) {
+		try {
+			String jsonConfig = cacheStore.get(key);
 			JsonToEntityConverter convertor = new JsonToEntityConverter();
 			Configuration config = (Configuration) convertor.convertjsonStringToEntity(jsonConfig);
 
@@ -27,23 +68,15 @@ public class Store {
 			return null;
 		}
 	}
+	
+	public static String getFromSnippetStore(String snippetType) {
+		try {
+			String snippetDesc = snippetStore.get(snippetType);
+			return snippetDesc;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-	public static Configuration getChildConfiguration(String instance, String dataConfig) {
-		Configuration parentConfig = getConfiguration(instance);
-		return (Configuration) parentConfig.getProperty(dataConfig);
-	}
-	
-	public static Configuration getChildConfiguration(Configuration conf, String dataConfig) {
-		return (Configuration) conf.getProperty(dataConfig);
-	}
-	
-	public static Configuration getFromCache(String infoKey) {
-		String jsonConfig = cacheStore.get(infoKey);
-		JsonToEntityConverter convertor = new JsonToEntityConverter();
-		Configuration config = (Configuration) convertor.convertjsonStringToEntity(jsonConfig);
-
-		return config;
-	}
-	
-	
 }
--- appops.platform.client/src/main/java/in/appops/showcase/web/gwt/dynamicsnippet/client/DynamicSnippetEntryPoint.java
+++ appops.platform.client/src/main/java/in/appops/showcase/web/gwt/dynamicsnippet/client/DynamicSnippetEntryPoint.java
@@ -2,9 +2,11 @@ package in.appops.showcase.web.gwt.dynamicsnippet.client;
 
 import in.appops.client.common.config.cache.GlobalEntityCache;
 import in.appops.client.common.config.dsnip.ApplicationContext;
-import in.appops.client.common.config.dsnip.PageProcessor;
-import in.appops.client.common.config.util.ReusableSnippetStore;
+import in.appops.client.common.config.dsnip.HTMLSnippetPresenter;
+import in.appops.client.common.config.dsnip.PageSnippetPresenter;
+import in.appops.client.common.config.dsnip.SnippetGenerator;
 import in.appops.client.common.config.util.Store;
+import in.appops.client.common.gin.AppOpsGinjector;
 import in.appops.platform.core.entity.Entity;
 import in.appops.platform.core.shared.Configuration;
 
@@ -12,31 +14,48 @@ import java.util.ArrayList;
 import java.util.Map;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Element;
-import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.core.client.GWT;
 
 public class DynamicSnippetEntryPoint implements EntryPoint{
-
+	
+	private final String PAGESNIPPET = "pageSnippet";
+	
 	@Override
 	public void onModuleLoad() {
-		/**
-		 * Load Configuration in a dictionary.
-		 */
-		Store.loadStore();
+		
+		init();
+		start();
+	}
 
+	private void start() {
 		/**
-		 * Load all the reusable snippet descriptions to a dictionary.
+		 * From the html page get the content. All the content intended for
+		 * processing should be enclosed in an element with an id. With
+		 * reference to this id the content would be extracted and give to the
+		 * page processor.
 		 */
-		ReusableSnippetStore.loadSnippetDesc();
-		
-		
-		/**
-		 * Load Cache
-		 */
-		Configuration queryCacheConfiguration = Store.getFromCache("querycache");
+/*		Element pageElement = RootPanel.get("page").getElement();
+		String pageDescription = pageElement.getInnerHTML();
+
+		PageProcessor processor = new PageProcessor(pageDescription);
+		pageElement.setInnerHTML("");
+
+		RootPanel.get("art-main").add(processor);
+		processor.processPageDescription();		*/
+
+		AppOpsGinjector injector = GWT.create(AppOpsGinjector.class);
+		SnippetGenerator snippetGenerator = (SnippetGenerator)injector.getSnippetGenerator();
+
+		PageSnippetPresenter snippetPres = snippetGenerator.requestPageSnippet();
+		snippetPres.createPage();
+	}
+
+	private void init() {
+		Store.loadStore();
+		Configuration queryCacheConfiguration = Store.getFromCacheStore("querycache");
 		Map<String, ArrayList<String>> queryCache = (Map<String, ArrayList<String>>) queryCacheConfiguration.getPropertyByName("cache");
 
-		Configuration entityCacheConfiguration = Store.getFromCache("entitycache");
+		Configuration entityCacheConfiguration = Store.getFromCacheStore("entitycache");
 		Map<String, Entity> entityCache = (Map<String, Entity>) entityCacheConfiguration.getPropertyByName("cache");
 
 		GlobalEntityCache globalEntityCache = GlobalEntityCache.getInstance();
@@ -46,7 +65,7 @@ public class DynamicSnippetEntryPoint implements EntryPoint{
 		/**
 		 * Add application properties to ApplicationContext
 		 */
-		Configuration appContextConfig = Store.getConfiguration("applicationContext");
+		Configuration appContextConfig = Store.getFromConfigurationStore("applicationContext");
 		if(appContextConfig != null) {
 			ArrayList<String> contextParamList = appContextConfig.getPropertyByName("contextparam");
 	
@@ -54,73 +73,6 @@ public class DynamicSnippetEntryPoint implements EntryPoint{
 				ApplicationContext.getInstance().setPropertyByName(param, null);
 			}
 		}
-
-		/**
-		 * From the html page get the content. All the content intended for
-		 * processing should be enclosed in an element with an id. With
-		 * reference to this id the content would be extracted and give to the
-		 * page processor.
-		 */
-		Element rootEle = RootPanel.get("art-main").getElement();
-		String htmlDesc = rootEle.getInnerHTML();
-
-		PageProcessor processor = new PageProcessor(htmlDesc);
-		rootEle.setInnerHTML("");
-
-		RootPanel.get("art-main").add(processor);
-
-		processor.processPageDescription();
 	}
 	
-	/*@SuppressWarnings("unchecked")
-	@Override
-	public void onModuleLoad() {
-		DefaultExceptionHandler exceptionHandler = new DefaultExceptionHandler();
-		DispatchAsync	dispatch = new StandardDispatchAsync(exceptionHandler);
-		
-		String queryName = "getAllBlogs";
-		
-		Query query = new Query();
-		query.setQueryName(queryName);
-		
-		
-		Map<String, Serializable> queryParam = new HashMap<String, Serializable>();
-		queryParam.put("query", query);
-		
-		StandardAction action = new StandardAction(EntityList.class, "blogging.BloggingService.getArticles", queryParam);
-		
-		final HashMap<String, ArrayList<String>> queryCacheMap = new HashMap<String, ArrayList<String>>();
-		final Configuration entityCacheConfig = new Configuration();
-		entityCacheConfig.setType(new MetaType("config"));
-
-		final ArrayList<String> entityPointerList = new ArrayList<String>();
-		
-		final GlobalEntityCache globalEntityCache = GlobalEntityCache.getInstance();
-		
-		final String queryPointer = globalEntityCache.getQueryIdentifier(query);
-
-		dispatch.execute(action, new AsyncCallback<Result<EntityList>>() {
-
-			@Override
-			public void onFailure(Throwable caught) {
-				caught.printStackTrace();
-			}
-
-			@Override
-			public void onSuccess(Result<EntityList> result) {
-				EntityList entList = (EntityList) result.getOperationResult();
-				//TODO
-				if(entList != null) {
-					for(Entity entity : entList) {
-						String entityPointer = globalEntityCache.getEntityIdentifer(entity);
-						entityCacheConfig.setProperty(entityPointer, entity);
-					}
-					JSONObject entityCacheJson = EntityToJsonClientConvertor.createJsonFromEntity(entityCacheConfig);
-					System.out.println(entityCacheJson.toString());
-				}
-			
-			}
-		});
-	}
-*/
 }
