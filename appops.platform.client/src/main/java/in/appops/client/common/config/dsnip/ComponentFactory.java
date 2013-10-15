61ade4e56938eb4c2ca0629a80e44e448385c487
nitish <nitish@ensarm.com>
2013-10-15 18:56:13 +0530

    Removing Unwanted classes

--- appops.platform.client/src/main/java/in/appops/client/common/config/dsnip/ComponentFactory.java
+++
@@ -1,9 +0,0 @@
-package in.appops.client.common.config.dsnip;
-
-import in.appops.client.common.config.component.base.BaseComponentPresenter;
-import in.appops.client.common.config.field.BaseField;
-
-public interface ComponentFactory {
-	BaseField getField(String string);
-	BaseComponentPresenter getComponent(String type);
-}
