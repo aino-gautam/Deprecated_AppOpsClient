50a8379e46366b746e4c96a84023a65f2fd1c7af
nitish <nitish@ensarm.com>
2013-10-17 11:52:36 +0530

    Restructuring of ListComponent Mvp

--- appops.platform.client/src/main/java/in/appops/client/common/config/component/list/ListComponentPresenter.java
+++ appops.platform.client/src/main/java/in/appops/client/common/config/component/list/ListComponentPresenter.java
@@ -3,94 +3,36 @@ package in.appops.client.common.config.component.list;
 import in.appops.client.common.config.component.base.BaseComponent.BaseComponentConstant;
 import in.appops.client.common.config.component.base.BaseComponentPresenter;
 import in.appops.client.common.config.model.ConfigurationListModel;
-import in.appops.client.common.config.model.EntityListModel;
-import in.appops.client.common.core.EntityListReceiver;
-import in.appops.platform.core.entity.Entity;
-import in.appops.platform.core.util.EntityList;
 
-public class ListComponentPresenter extends BaseComponentPresenter implements EntityListReceiver {
-	private EntityListModel entityListModel;
+public class ListComponentPresenter extends BaseComponentPresenter {
+	
+	public ListComponentPresenter(String type, String instance) {
+		super(type, instance);
+	}
 	
 	@Override
 	public void initialize() {
-		model = new ConfigurationListModel();
-		entityListModel = (EntityListModel) model;
-		entityListModel.setReceiver(this);
-		view = new ListComponentView();
+		model = (ConfigurationListModel) dynamicFactory.requestModel(type);
+		view = dynamicFactory.requestView(type);
+		view.setModel(model);
+		view.initialize();
 	}
 
-	/*@Override
-	public void load() {
-		entityListModel.fetchEntityList();
-	}*/
-	
-/*	
-	@SuppressWarnings("unchecked")
-	protected HashMap<String, Configuration> getInterestedEvents() {
-		HashMap<String, Configuration> interestedEvents = new HashMap<String, Configuration>();
-		if(getConfigurationValue(ContainerConstant.CT_INTRSDEVNTS) != null) {
-			interestedEvents = (HashMap<String, Configuration>) getConfigurationValue(ContainerConstant.CT_INTRSDEVNTS);
-		}
-		return interestedEvents;
-	}
-	
-	
-	public boolean isTypeInteresting(String eventName) {
-		HashMap<String, Configuration> interestedEvents = getInterestedEvents();
-		Set<String> eventSet = interestedEvents.keySet();
+	@Override
+	public void create() {
+		super.create();
 		
-		if(!eventSet.isEmpty()) {
-			if(eventSet.contains(eventName)) {
-				return true;
+		if(model.getModelConfiguration() != null) {		
+			String operationName = ((ConfigurationListModel) model).getOperationName();
+			if(operationName != null && !operationName.equals("")) {
+				((ConfigurationListModel) model).fetchEntityList();
 			}
 		}
-		return false;
 	}
-	
-	protected Configuration getEventConfiguration(String event) {
-		HashMap<String, Configuration> interestedEvents = getInterestedEvents();
-		
-		if(!interestedEvents.isEmpty() && interestedEvents.containsKey(event)) {
-			return interestedEvents.get(event);
-		}
-		return null;
-	}*/
-	
-	
-	
-
-	@Override
-	public void noMoreData() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onEntityListReceived(EntityList entityList) {
-		if(entityList != null && !entityList.isEmpty()) {
-			ListComponentView listView = (ListComponentView)view;
-			listView.setEntityList(entityList);
-			listView.populate();
-		}
-	}
-
-	@Override
-	public void onEntityListUpdated() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void updateCurrentView(Entity entity) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 	
 	public interface ListComponentConstant extends BaseComponentConstant {
 		String LC_LISTCLS = "listCss";
 		String LC_SNIPPETTYPE = "snippetType";
 		String LC_INSTANCETYPE = "snippetInstance";
 	}
-
 }
--- appops.platform.client/src/main/java/in/appops/client/common/config/component/list/ListComponentView.java
+++ appops.platform.client/src/main/java/in/appops/client/common/config/component/list/ListComponentView.java
@@ -3,57 +3,35 @@ package in.appops.client.common.config.component.list;
 import in.appops.client.common.config.component.base.BaseComponent;
 import in.appops.client.common.config.component.list.ListComponentPresenter.ListComponentConstant;
 import in.appops.client.common.config.dsnip.HTMLSnippetPresenter;
-import in.appops.client.common.config.dsnip.DynamicMVPFactory;
-import in.appops.client.common.gin.AppOpsGinjector;
+import in.appops.client.common.config.model.ConfigurationListModel;
+import in.appops.client.common.config.model.ConfigurationModel;
+import in.appops.client.common.core.EntityListReceiver;
 import in.appops.platform.core.entity.Entity;
 import in.appops.platform.core.util.EntityList;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HasAlignment;
 import com.google.gwt.user.client.ui.ScrollPanel;
 
-public class ListComponentView extends BaseComponent  {
+public class ListComponentView extends BaseComponent implements EntityListReceiver {
 	private ScrollPanel scrollPanel;
 	protected FlexTable listPanel;
 	private int row = 0;
-	private String instanceType;
-	private String snippetType;
-	private EntityList entityList;
-
-
-	public ListComponentView() {
-		super();
-	}
-
 
 	@Override
 	public void initialize() {
 		super.initialize();
 		listPanel = new FlexTable();
 		scrollPanel = new ScrollPanel(listPanel);
+		((ConfigurationListModel)model).setReceiver(this);
 	}
 	
 	@Override
 	public void configure() {
 		super.configure();
-		
 		listPanel.setStylePrimaryName(getListPanelPrimCss());
-		
-		setInstanceType(getInstanceType());
-		setSnippetType(getSnippetType());
 	}
-	
-	public void setInstanceType(String instanceType) {
-		this.instanceType = instanceType;
-	}
-
-
-	public void setSnippetType(String snippetType) {
-		this.snippetType = snippetType;
-	}
-
 
 	private String getListPanelPrimCss() {
 		String primaryCss = "appops-ListScrollCss";
@@ -72,52 +50,30 @@ public class ListComponentView extends BaseComponent  {
 		scrollPanel.setAlwaysShowScrollBars(false);
 	}
 
-	
-	
-	protected void populate() {
+	private HTMLSnippetPresenter getChildSnippet() {
+		HTMLSnippetPresenter snippetPres = mvpFactory.requestHTMLSnippet(((ConfigurationListModel)model).getSnippetType(), ((ConfigurationListModel)model).getSnippetInstance());
+		return snippetPres;
+	}
+
+	@Override
+	public void onEntityListReceived(EntityList entityList) {
 		listPanel.clear();
 		row = 0;
 		for(Entity entity : entityList){
 			HTMLSnippetPresenter snippetPres  = getChildSnippet();
-			listPanel.setWidget(row ,0 ,snippetPres.getView());
-//			snippetPres.setEntity(entity);
-//			snippetPres.load();
+			((ConfigurationModel)snippetPres.getModel()).setEntity(entity);
+			snippetPres.create();
+			listPanel.setWidget(row, 0, snippetPres.getView());
 			row++;
-		}
+		}		
 	}
 
-	private HTMLSnippetPresenter getChildSnippet() {
-		AppOpsGinjector injector = GWT.create(AppOpsGinjector.class);
-		DynamicMVPFactory snippetGenerator = (DynamicMVPFactory)injector.getMVPFactory();
-		HTMLSnippetPresenter snippetPres = snippetGenerator.requestHTMLSnippet(snippetType, instanceType);
-		return snippetPres;
-	}
+	@Override
+	public void onEntityListUpdated() {}
 
+	@Override
+	public void updateCurrentView(Entity entity) {	}
 
-	private String getInstanceType() {
-		String instanceType = null;
-		if(viewConfiguration.getConfigurationValue(ListComponentConstant.LC_INSTANCETYPE) != null) {
-			instanceType = viewConfiguration.getConfigurationValue(ListComponentConstant.LC_INSTANCETYPE).toString();
-		}
-		return instanceType;
-	}
-
-
-	private String getSnippetType() {
-		String snippetType = "appops-ListScrollCss";
-		if(viewConfiguration.getConfigurationValue(ListComponentConstant.LC_SNIPPETTYPE) != null) {
-			snippetType = viewConfiguration.getConfigurationValue(ListComponentConstant.LC_SNIPPETTYPE).toString();
-		}
-		return snippetType;
-	}
-
-
-	public EntityList getEntityList() {
-		return entityList;
-	}
-
-
-	public void setEntityList(EntityList entityList) {
-		this.entityList = entityList;
-	}
+	@Override
+	public void noMoreData() {	}
 }
