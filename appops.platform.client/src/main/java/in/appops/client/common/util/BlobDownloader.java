2e3594559d0d34d5dbb8df2d5b3e431c85365fe5
MaheshMore <mahesh@ensarm.com>
2013-12-12 11:12:34 +0530

    changes added to make the image loading work with phonegap

--- appops.platform.client/src/main/java/in/appops/client/common/util/BlobDownloader.java
+++ appops.platform.client/src/main/java/in/appops/client/common/util/BlobDownloader.java
@@ -1,20 +1,33 @@
 package in.appops.client.common.util;
 
 import com.google.gwt.core.client.GWT;
+import com.googlecode.gwtphonegap.client.PhoneGap;
 
 public class BlobDownloader {
 	
+	private static final PhoneGap phoneGap = GWT.create( PhoneGap.class );
+	
 	 public String getImageDownloadURL(String blobId){
-		return GWT.getHostPageBaseURL()+"download?blobId="+blobId;
+		 if(phoneGap.isPhoneGapDevice()){
+			 return "http://server.ensarm.com:4080/Trumart/"+"download?blobId="+blobId;
+		 }
+		 else
+			 return GWT.getHostPageBaseURL()+"download?blobId="+blobId;
 		 
 	 }
 	 public String getIconDownloadURL(String blobId){
-		 return GWT.getHostPageBaseURL()+"download?blobId=i_"+blobId;
-		 
+		 if(phoneGap.isPhoneGapDevice()){
+			 return "http://server.ensarm.com:4080/Trumart/"+"download?blobId=i_"+blobId;
+		 }
+		 else
+			 return GWT.getHostPageBaseURL()+"download?blobId=i_"+blobId;
 	 }
 
 	 public String getThumbNailDownloadURL(String blobId){
-		 return GWT.getHostPageBaseURL()+"download?blobId=t_"+blobId;
-		 
+		 if(phoneGap.isPhoneGapDevice()){
+			 return "http://server.ensarm.com:4080/Trumart/"+"download?blobId=t_"+blobId;
+		 }
+		 else
+			 return GWT.getHostPageBaseURL()+"download?blobId=t_"+blobId;
 	 }
 }
